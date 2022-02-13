afa205640cc97a2ac6f6a7e5582b15d34c1cf0c3
C.Renauld Rodney <c.renauld.rodney@gmail.com>
2022-02-13 07:54:46 -0600

    return JS file to original state

--- assets/js/starbucks.js
+++ assets/js/starbucks.js
@@ -1,14 +1,14 @@
-$(function () {
-  $(".section-title").css({
-    cursor: "pointer",
-  });
-  // hide .section elements
-  $(".section-elements").hide();
+// $(function () {
+//   $(".section-title").css({
+//     cursor: "pointer",
+//   });
+//   // hide .section elements
+//   $(".section-elements").hide();
 
-  // slide down section elements when section title clicked
-  $(".about-us__title").click(function () {
-    $(".about-us__elements").slideToggle(function () {});
-  });
-});
+//   // slide down section elements when section title clicked
+//   $(".section-title").click(function () {
+//     $(".section-elements").slideToggle(function () {});
+//   });
+// });
 
 /*DO NOT DELETE THIS- THIS IS WORKING JQUERY CODE TO BE REFACTORED!!* */
