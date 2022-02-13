ad1a3c805e52a3708963e8ac87c301515d3725ef
C.Renauld Rodney <c.renauld.rodney@gmail.com>
2022-02-13 16:55:09 -0600

    '.section-elements' items will hide/ show based on browser width

--- assets/js/starbucks.js
+++ assets/js/starbucks.js
@@ -5,10 +5,33 @@
 //   // hide .section elements
 //   $(".section-elements").hide();
 
-//   // slide down section elements when section title clicked
-//   $(".section-title").click(function () {
-//     $(".section-elements").slideToggle(function () {});
+//   // slide down section elements when section title clicked/toggled
+//   $(".about-us__title").click(function () {
+//     $(".about-us__elements").slideToggle(function () {});
 //   });
 // });
 
 /*DO NOT DELETE THIS- THIS IS WORKING JQUERY CODE TO BE REFACTORED!!* */
+
+/* create function to trigger jquery */
+function runJquery(browserWidth) {
+  // unhide section elements if browser width >= 768px
+  if (browserWidth.matches) {
+    $(".section-elements").show();
+  }
+
+  // hide section elements if browser width < 768px
+  else {
+    $(".section-elements").hide();
+  }
+}
+
+/* media query list objbect */
+const browserWidth = window.matchMedia("(min-width: 768px)");
+// console.log(browserWidth);
+
+// invoke runJquery function
+runJquery(browserWidth);
+
+// eventListener added to mediaQueryList object to listen for changes in browser width
+browserWidth.addListener(runJquery); // addListener() deprecated? what replaced it?
