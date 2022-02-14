f83e85d308c8d67522438ccb7ffbb778a94b585d
C.Renauld Rodney <c.renauld.rodney@gmail.com>
2022-02-14 11:39:14 -0600

    added appropriate classes to HTML + added necessary jquery attachments to JS to allow JS to control when section elements are shown and hidden

--- assets/js/starbucks.js
+++ assets/js/starbucks.js
@@ -18,15 +18,39 @@ function runJquery(browserWidth) {
   // unhide section elements if browser width >= 768px
   if (browserWidth.matches) {
     $(".section-elements").show();
-  }
-
-  // hide section elements if browser width < 768px
-  else {
+  } else {
+    /* hide section elements if browser width < 768px +// show section
+  elements when section title is clicked */
     $(".section-elements").hide();
+
+    // about us
+    $(".about-us__title").click(function () {
+      $(".about-us__elements").slideToggle(function () {});
+    });
+
+    // careers
+    $(".careers__title").click(function () {
+      $(".careers__elements").slideToggle(function () {});
+    });
+
+    // social impact
+    $(".social-impact__title").click(function () {
+      $(".social-impact__elements").slideToggle(function () {});
+    });
+
+    // business partners
+    $(".business-partners__title").click(function () {
+      $(".business-partners__elements").slideToggle(function () {});
+    });
+
+    // order and pickup
+    $(".order-pickup__title").click(function () {
+      $(".order-pickup__elements").slideToggle(function () {});
+    });
   }
 }
 
-/* media query list objbect */
+/* media query list objbect stores media query"min-width:768px... */
 const browserWidth = window.matchMedia("(min-width: 768px)");
 // console.log(browserWidth);
 
