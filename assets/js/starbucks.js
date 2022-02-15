91e943eab4d13e43efb61b71da8fcd6e6042f867
C.Renauld Rodney <c.renauld.rodney@gmail.com>
2022-02-15 08:59:21 -0600

    jQuery shows/ hides section-elements when specific conditions are met

--- assets/js/starbucks.js
+++ assets/js/starbucks.js
@@ -14,7 +14,10 @@
 
 // function to manage behavior @ widths >768px
 function wideScreen(browserWidth) {
-  console.log("> 768px...");
+  console.log(">= 768px...");
+  $(function () {
+    $(".section-elements").show();
+  });
 }
 
 /*init function runs mobile first styles + jquery library @ page laod .
@@ -23,9 +26,11 @@ function wideScreen(browserWidth) {
 function init(browserWidth) {
   // run jqeury - ONLY if browser width to be less than 768px!
   if (browserWidth.matches) {
-    console.log("browser <= 768px");
-    // run jquery
-    console.log($(function () {}));
+    console.log("browser < 768px");
+    // run jquery + hide section elements
+    $(function () {
+      $(".section-elements").hide();
+    });
   } else {
     wideScreen(browserWidth);
   }
@@ -35,36 +40,35 @@ function init(browserWidth) {
 
 /* hide section elements if browser width < 768px +// show section
   elements when section title is clicked */
-$(".section-elements").hide();
 
 // about us
-$(".about-us__title").click(function () {
-  $(".about-us__elements").slideToggle(function () {});
-});
+// $(".about-us__title").click(function () {
+//   $(".about-us__elements").slideToggle(function () {});
+// });
 
 // careers
-$(".careers__title").click(function () {
-  $(".careers__elements").slideToggle(function () {});
-});
+// $(".careers__title").click(function () {
+//   $(".careers__elements").slideToggle(function () {});
+// });
 
 // social impact
-$(".social-impact__title").click(function () {
-  $(".social-impact__elements").slideToggle(function () {});
-});
+// $(".social-impact__title").click(function () {
+//   $(".social-impact__elements").slideToggle(function () {});
+// });
 
 // business partners
-$(".business-partners__title").click(function () {
-  $(".business-partners__elements").slideToggle(function () {});
-});
+// $(".business-partners__title").click(function () {
+//   $(".business-partners__elements").slideToggle(function () {});
+// });
 
 // order and pickup
-$(".order-pickup__title").click(function () {
-  $(".order-pickup__elements").slideToggle(function () {});
-});
+// $(".order-pickup__title").click(function () {
+//   $(".order-pickup__elements").slideToggle(function () {});
+// });
 
 /* media query list objbect stores media query"max-width:768px...
 for mobile first */
-let browserWidth = window.matchMedia("(max-width: 768px)");
+let browserWidth = window.matchMedia("(max-width: 767px)");
 // console.log(browserWidth);
 
 // invoke function
