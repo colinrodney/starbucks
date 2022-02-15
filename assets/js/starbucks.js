118a94bf300ad6dc25a59887b55a95be948126e8
C.Renauld Rodney <c.renauld.rodney@gmail.com>
2022-02-15 08:51:34 -0600

    split runJquery function into two separate functions whereas init function manages mobile styles + triggers jQuery and wideScreen function manages non-mobile styles and behaviorsand DOES NOT trigger jQuery

--- assets/js/starbucks.js
+++ assets/js/starbucks.js
@@ -1,3 +1,4 @@
+/*DO NOT DELETE THIS- THIS IS WORKING JQUERY CODE TO BE REFACTORED!!* */
 // $(function () {
 //   $(".section-title").css({
 //     cursor: "pointer",
@@ -11,51 +12,64 @@
 //   });
 // });
 
-/*DO NOT DELETE THIS- THIS IS WORKING JQUERY CODE TO BE REFACTORED!!* */
+// function to manage behavior @ widths >768px
+function wideScreen(browserWidth) {
+  console.log("> 768px...");
+}
 
-/* create function to trigger jquery */
-function runJquery(browserWidth) {
-  // unhide section elements if browser width >= 768px
+/*init function runs mobile first styles + jquery library @ page laod .
+- will call the wideScreenfunction if browserWidth does not meet condition
+*/
+function init(browserWidth) {
+  // run jqeury - ONLY if browser width to be less than 768px!
   if (browserWidth.matches) {
-    $(".section-elements").show();
+    console.log("browser <= 768px");
+    // run jquery
+    console.log($(function () {}));
   } else {
-    /* hide section elements if browser width < 768px +// show section
-  elements when section title is clicked */
-    $(".section-elements").hide();
-
-    // about us
-    $(".about-us__title").click(function () {
-      $(".about-us__elements").slideToggle(function () {});
-    });
-
-    // careers
-    $(".careers__title").click(function () {
-      $(".careers__elements").slideToggle(function () {});
-    });
-
-    // social impact
-    $(".social-impact__title").click(function () {
-      $(".social-impact__elements").slideToggle(function () {});
-    });
-
-    // business partners
-    $(".business-partners__title").click(function () {
-      $(".business-partners__elements").slideToggle(function () {});
-    });
-
-    // order and pickup
-    $(".order-pickup__title").click(function () {
-      $(".order-pickup__elements").slideToggle(function () {});
-    });
+    wideScreen(browserWidth);
   }
 }
 
-/* media query list objbect stores media query"min-width:768px... */
-const browserWidth = window.matchMedia("(min-width: 768px)");
+/* create function to trigger jquery */
+
+/* hide section elements if browser width < 768px +// show section
+  elements when section title is clicked */
+$(".section-elements").hide();
+
+// about us
+$(".about-us__title").click(function () {
+  $(".about-us__elements").slideToggle(function () {});
+});
+
+// careers
+$(".careers__title").click(function () {
+  $(".careers__elements").slideToggle(function () {});
+});
+
+// social impact
+$(".social-impact__title").click(function () {
+  $(".social-impact__elements").slideToggle(function () {});
+});
+
+// business partners
+$(".business-partners__title").click(function () {
+  $(".business-partners__elements").slideToggle(function () {});
+});
+
+// order and pickup
+$(".order-pickup__title").click(function () {
+  $(".order-pickup__elements").slideToggle(function () {});
+});
+
+/* media query list objbect stores media query"max-width:768px...
+for mobile first */
+let browserWidth = window.matchMedia("(max-width: 768px)");
 // console.log(browserWidth);
 
-// invoke runJquery function
-runJquery(browserWidth);
+// invoke function
+init(browserWidth);
+// runJquery(browserWidth);
 
 // eventListener added to mediaQueryList object to listen for changes in browser width
-browserWidth.addListener(runJquery); // addListener() deprecated? what replaced it?
+browserWidth.addListener(init); // addListener() deprecated? what replaced it?
